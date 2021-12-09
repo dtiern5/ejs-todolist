c70ef103bdf862a04ba8a6d8f3616629d18bdc1a
dtiern5 <dannyjtierney@gmail.com>
2021-12-09 10:18:38 -0600

    css added

--- app.js
+++ app.js
@@ -2,28 +2,29 @@ const express = require("express");
 
 const app = express();
 
-var items = [];
+let items = [];
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {kindOfDay: day, newListItems: items});
 });
 
 app.post("/", (req, res) => {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
 
   items.push(item);
 
