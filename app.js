279c7a74c49750d34c583658df176bdd6e26768f
dtiern5 <dannyjtierney@gmail.com>
2021-12-11 12:26:32 -0600

    restored default items

--- app.js
+++ app.js
@@ -21,7 +21,17 @@ const listSchema = {
 };
 const List = mongoose.model("List", listSchema)
 
-3
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+const item2 = new Item({
+  name: "Hit the '+' button to add a new item."
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+const defaultItems = [item1, item2, item3];
+
 app.get("/", (req, res) => {
   Item.find({}, (err, foundItems) => {
     if (foundItems.length === 0) {
